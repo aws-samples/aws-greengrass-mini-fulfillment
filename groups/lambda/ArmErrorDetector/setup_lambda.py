86e72acec564c725b54a439a3bf3d0040b3befd8
Brett Francis <brett_francis@me.com>
2017-05-31 15:06:40 -0700

    initial checkin

---
+++ groups/lambda/ArmErrorDetector/setup_lambda.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not
+# use this file except in compliance with the License. A copy of the License is
+# located at
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from __future__ import print_function
+import os
+import json
+import zipfile
+import boto3
+from botocore.exceptions import ClientError
+from retrying import retry
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print("path: {0}".format(dir_path))
+
+
+def create(func_name, runtime='python2.7', role_name='NoServiceAccess',
+           assume_role_policy_doc='trust.json', lambda_main=None,
+           lambda_handler='handler', lambda_files=None):
+    iam = boto3.client('iam')
+    role_arn = ''
+    try:
+        with open(name=assume_role_policy_doc) as trust_file:
+            trust = json.dumps(json.load(trust_file))
+            resp = iam.create_role(RoleName=role_name,
+                                   # Path=dir_path+'/',
+                                   AssumeRolePolicyDocument=trust)
+            role_arn = resp['Role']['Arn']
+
+        print('created iam role:{0} with arn:{1}'.format(role_name, role_arn))
+    except ClientError as ce:
+        if ce.response['Error']['Code'] == 'EntityAlreadyExists':
+            print("Role '{0}' already exists. Using existing Role".format(
+                role_name))
+            role = iam.get_role(RoleName=role_name)
+            role_arn = role['Role']['Arn']
+        else:
+            print("Unexpected Error: {0}".format(ce))
+
+    try:
+        with open(name='policy.json') as policy_file:
+            policy = json.dumps(json.load(policy_file))
+            resp = iam.put_role_policy(RoleName=role_name,
+                                       PolicyName=func_name+'_policy',
+                                       PolicyDocument=policy)
+    except ClientError as ce:
+        if ce.response['Error']['Code'] == 'EntityAlreadyExists':
+            print("Policy '{0}' already exists.".format(role_name))
+        else:
+            print("Unexpected Error: {0}".format(ce))
+
+    with zipfile.ZipFile("deploy.zip", "w", zipfile.ZIP_DEFLATED) as zf:
+        for f in lambda_files:
+            zf.write(f)
+
+    _create_lambda(role_arn, func_name, lambda_handler, lambda_main, runtime)
+
+
+@retry(wait_random_min=4000, wait_random_max=6000, stop_max_attempt_number=3)
+def _create_lambda(arn, func_name, lambda_handler, lambda_main,
+                   runtime):
+    func = dict()
+    lamb = boto3.client('lambda')
+    with open('deploy.zip') as deploy:
+        func['ZipFile'] = deploy.read()
+    try:
+        resp = lamb.create_function(
+            FunctionName=func_name, Runtime=runtime, Publish=True,
+            Description="gg-mini-fulfillment function for arm error detection",
+            Role=arn, Code=func, Handler='{0}.{1}'.format(
+                lambda_main, lambda_handler
+            ))
+        print("Create Lambda Function resp:{0}".format(resp))
+    except ClientError as ce:
+        if ce.response['Error']['Code'] == 'ValidationException':
+            print("Validation Error {0} creating function '{1}'.".format(
+                ce, func_name))
+        else:
+            print("Unexpected Error: {0}".format(ce))
+
+
+def update(func_name, lambda_main=None, lambda_handler='handler',
+           lambda_files=None):
+    pass
+
+if __name__ == "__main__":
+    create(
+        func_name='ArmErrorDetector',
+        lambda_main='error_detector',
+        lambda_files=['error_detector.py']
+    )
+    # update(
+    #     func_name='SHSortArmErrorDetector',
+    #     lambda_main='error_detector',
+    #     lambda_files=['error_detector.py']
+    # )
---
+++ groups/lambda/MasterBrain/master_brain.py
@@ -0,0 +1,156 @@
+from __future__ import print_function
+import json
+import logging
+import greengrasssdk
+
+log = logging.getLogger('brain')
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s|%(name)-8s|%(levelname)s: %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.INFO)
+
+GGC_THING_NAME = "MasterBrain"
+
+gg_client = greengrasssdk.client('iot-data')
+
+
+def handle_arm_stage(ggd_id, msg):
+    if ggd_id == "GGD_sort_arm":
+        print("[handle_arm_stage] message from the sort arm")
+        stage = msg['stage']
+        addl_text = msg['addl_text']
+        if stage == 'pick' and addl_text == 'begin':
+            log.info("[handle_arm_stage] GGD_sort_arm PICK BEGIN >> CHANGE DIRECTION")
+            # Conveying "away" from sort arm is convey_reverse=False
+            gg_client.update_thing_shadow(
+                thingName=GGC_THING_NAME, payload=json.dumps({
+                    "state": {
+                        "desired": {
+                            "convey_reverse": False
+                        }
+                    }
+                }).encode()
+            )
+    elif ggd_id == "GGD_inv_arm":
+        log.info("[handle_arm_stage] message from the inv arm")
+        stage = msg['stage']
+        addl_text = msg['addl_text']
+        if stage == 'pick' and addl_text == 'begin':
+            log.info(
+                "[handle_arm_stage] GGD_inv_arm PICK BEGIN >> CHANGE DIRECTION"
+            )
+            # Conveying "away" from inv arm is convey_reverse=True
+            gg_client.update_thing_shadow(
+                thingName=GGC_THING_NAME, payload=json.dumps({
+                    "state": {
+                        "desired": {
+                            "convey_reverse": True
+                        }
+                    }
+                }).encode()
+            )
+    else:
+        log.error("[handle_arm_stage] couldn't understand stage msg:{0}".format(
+            msg
+        ))
+
+
+def handle_button(msg):
+    button_id = msg['data'][0]['sensor_id']
+    value = msg['data'][0]['value']
+    log.info("[handle_button] button id:'{0}' value:'{1}'".format(
+        button_id, value))
+    if button_id == 'green-button' and value == 'on':
+        log.info(
+            "[handle_button] button id:'{0}' start_cmd".format(button_id))
+        gg_client.update_thing_shadow(
+            thingName=GGC_THING_NAME, payload=json.dumps({
+                "state": {
+                    "desired": {
+                        "convey_cmd": "run",
+                        "sort_arm_cmd": "run",
+                        "inv_arm_cmd": "run",
+                    }
+                }
+            }).encode()
+        )
+    elif button_id == 'red-button' and value == 'on':
+        log.info(
+            "[handle_button] button id:'{0}' stop_cmd".format(button_id))
+        gg_client.update_thing_shadow(
+            thingName=GGC_THING_NAME, payload=json.dumps({
+                "state": {
+                    "desired": {
+                        "convey_cmd": "stop",
+                        "sort_arm_cmd": "stop",
+                        "inv_arm_cmd": "stop",
+                    }
+                }
+            }).encode()
+        )
+    elif button_id == 'white-button' and value == 'on':
+        log.info(
+            "[handle_button] button id:'{0}' convey_reverse=True".format(
+                button_id
+            ))
+        gg_client.update_thing_shadow(
+            thingName=GGC_THING_NAME, payload=json.dumps({
+                "state": {
+                    "desired": {
+                        "convey_reverse": 1
+                    }
+                }
+            }).encode()
+        )
+    elif button_id == 'white-button' and value == 'off':
+        log.info(
+            "[handle_button] button id:'{0}' convey_reverse=False".format(
+                button_id
+            ))
+        gg_client.update_thing_shadow(
+            thingName=GGC_THING_NAME, payload=json.dumps({
+                "state": {
+                    "desired": {
+                        "convey_reverse": 0
+                    }
+                }
+            }).encode()
+        )
+
+
+# Handler for processing lambda work items
+def handler(event, context):
+    # Unwrap the message
+    msg = json.loads(event)
+    log.info("[handler] thinking about message: {0}".format(msg))
+    log.debug("[handler] context.function_name:{0}".format(
+        context.function_name))
+    log.debug("[handler] context.client_context:{0}".format(
+        context.client_context))
+    # topic = context.client_context.custom['subject']
+
+    ggd_id = ''
+    if 'ggd_id' in msg:
+        ggd_id = msg['ggd_id']
+
+    if ggd_id == "GGD_button":
+        handle_button(msg)
+    elif ggd_id == "GGD_belt":
+        log.debug("[handler] message from the belts")
+    elif ggd_id == "GGD_bridge":
+        log.debug("[handler] message from the bridge")
+    elif ggd_id == "GGD_sort_arm":
+        log.debug("[handler] message from the sort arm")
+        if 'stage' in msg:
+            handle_arm_stage(ggd_id=ggd_id, msg=msg)
+    elif ggd_id == "GGD_inv_arm":
+        log.debug("[handler] message from the inv arm")
+        if 'stage' in msg:
+            handle_arm_stage(ggd_id=ggd_id, msg=msg)
+    else:
+        log.error("[handler] unknown ggd_id:{0}".format(ggd_id))
+
+    # TODO act upon errors received from each station
+
