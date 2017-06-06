c0d53629caa9253b72f82d6e2a2a93e234ca9b96
Brett Francis <brett_francis@me.com>
2017-06-05 21:15:08 -0700

    clean-up and use modules

--- groups/master/run_ggd.sh
+++ groups/master/run_ggd.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
 
-#cd ggd
-#chmod 755 belt.py
-#chmod 755 bridge.py
-#chmod 755 button.py
-#chmod 755 heartbeat.py
-#chmod 755 web.py
-#
-#screen -S belt -h 200 -d -m ./belt.py --frequency 0.1
-#screen -S bridge -h 200 -d -m ./bridge.py
-#screen -S button -h 200 -d -m ./button.py box
-#screen -S heartbeat -h 200 -d -m ./heartbeat.py
-#screen -S web -h 200 -d -m ./web.py
-#screen -ls
-
+cd ..
 screen -S belt -h 200 -d -m python -m master.ggd.belt master/cfg.tmp.json --frequency 0.1
 screen -S bridge -h 200 -d -m python -m master.ggd.bridge master/cfg.tmp.json
-screen -S button -h 200 -d -m python -m master.ggd.button master/cfg.tmp.json
+screen -S button -h 200 -d -m python -m master.ggd.button master/cfg.tmp.json box
 screen -S heartbeat -h 200 -d -m python -m master.ggd.heartbeat master/cfg.tmp.json
 screen -S web -h 200 -d -m python -m master.ggd.web master/cfg.tmp.json
 screen -ls
+cd master
