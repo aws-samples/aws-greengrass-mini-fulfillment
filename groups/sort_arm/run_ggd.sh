b08a9d3c523138097a8b888bbb2ef809b1db224c
Brett Francis <brett_francis@me.com>
2017-07-03 16:09:39 -0700

    fixed screen names

--- groups/sort_arm/run_ggd.sh
+++ groups/sort_arm/run_ggd.sh
@@ -5,6 +5,6 @@ chmod 755 load_gg_profile.sh
 ./load_gg_profile.sh
 
 cd ../..
-screen -S heartbeat -h 200 -d -m python -m sort_arm.ggd.arm sort_arm/cfg.tmp.json --frequency 0.1
-screen -S heartbeat -h 200 -d -m python -m sort_arm.ggd.heartbeat sort_arm/cfg.tmp.json
+screen -S arm -h 200 -d -m python -m sort_arm.ggd.arm sort_arm/cfg.tmp.json "ggd/certs/sort_arm-server.crt" --frequency 0.1
+screen -S heartbeat -h 200 -d -m python -m sort_arm.ggd.heartbeat sort_arm/cfg.tmp.json "ggd/certs/sort_arm-server.crt"
 screen -ls
