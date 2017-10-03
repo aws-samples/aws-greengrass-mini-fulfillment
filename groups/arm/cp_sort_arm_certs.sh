2f776e823afc415c864c3682fc3c8b82894b22c7
Brett Francis <brettf@amazon.com>
2017-10-03 15:21:24 -0700

    specific names

--- groups/arm/cp_sort_arm_certs.sh
+++ groups/arm/cp_sort_arm_certs.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 echo "[begin] copying Sort Arm GGC certs"
-#echo "sudo cp sort_arm/sort_arm-server.crt /greengrass/configuration/certs/server.crt"
-#sudo cp sort_arm/sort_arm-server.crt /greengrass/configuration/certs/server.crt
-#echo "sudo cp sort_arm/sort_arm-server-private.key /greengrass/configuration/certs/server.key"
-#sudo cp sort_arm/sort_arm-server-private.key /greengrass/configuration/certs/server.key
-#TODO echo "sudo cp sort_arm/cloud*.* /greengrass/configuration/certs"
-#TODO sudo cp sort_arm/cloud*.* /greengrass/configuration/certs
+echo "sudo cp sort_arm/sort_arm-core.pem /greengrass/certs"
+sudo cp sort_arm/sort_arm-core.pem /greengrass/certs
+echo "sudo cp sort_arm/sort_arm-core.prv /greengrass/certs"
+sudo cp sort_arm/sort_arm-core.prv /greengrass/certs
 echo "sudo cp sort_arm/root-ca.pem /greengrass/configuration/certs"
-sudo cp sort_arm/root-ca.pem /greengrass/configuration/certs
+sudo cp sort_arm/root-ca.pem /greengrass/certs
 echo "[end] copying certs"
