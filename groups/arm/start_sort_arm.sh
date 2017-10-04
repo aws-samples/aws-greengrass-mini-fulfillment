96f69470cd77fe2196ced870d96bc0e4ff5f401c
Brett Francis <brettf@amazon.com>
2017-10-03 21:00:21 -0700

    upgraded for discovery

--- groups/arm/start_sort_arm.sh
+++ groups/arm/start_sort_arm.sh
@@ -5,6 +5,16 @@ chmod 755 load_gg_profile.sh
 ./load_gg_profile.sh
 
 cd ../..
-screen -S arm -h 200 -d -m python -m arm.ggd.arm cfg.tmp.json "sort_arm/sort_arm-server.crt" --frequency 0.1
-screen -S heartbeat -h 200 -d -m python -m arm.ggd.heartbeat cfg.tmp.json "sort_arm/sort_arm-server.crt"
+screen -S arm -h 200 -d -m python -m arm.ggd.arm sort_arm_ggd \
+~/mini-fulfillment/groups/arm/sort_arm/cfg.json \
+~/mini-fulfillment/groups/arm/sort_arm/ggd_certs/root-ca.pem \
+~/mini-fulfillment/groups/arm/sort_arm/ggd_certs/sort_arm_ggd.pem \
+~/mini-fulfillment/groups/arm/sort_arm/ggd_certs/sort_arm_ggd.prv \
+~/mini-fulfillment/groups/arm/sort_arm/ggd_certs
+screen -S heartbeat -h 200 -d -m python -m arm.ggd.heartbeat sort_heartbeat_ggd \
+~/mini-fulfillment/groups/arm/sort_arm/cfg.json \
+~/mini-fulfillment/groups/arm/sort_arm/ggd_certs/root-ca.pem \
+~/mini-fulfillment/groups/arm/sort_arm/ggd_certs/sort_heartbeat_ggd.pem \
+~/mini-fulfillment/groups/arm/sort_arm/ggd_certs/sort_heartbeat_ggd.prv \
+~/mini-fulfillment/groups/arm/sort_arm/ggd_certs --frequency 0.1
 screen -ls
