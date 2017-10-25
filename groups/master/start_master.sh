be2c9cba17eb37b7f4b7651c64db3df75fc949d7
Brett Francis <brettf@amazon.com>
2017-10-24 22:06:11 -0700

    shifting to discovery-based command line

--- groups/master/start_master.sh
+++ groups/master/start_master.sh
@@ -1,10 +1,33 @@
 #!/usr/bin/env bash
 
-cd ..
-screen -S belt -h 200 -d -m python -m master.ggd.belt master/cfg.tmp.json --frequency 0.1
+cur_dir=$(pwd)
+
+cd ggd
+chmod 755 load_gg_profile.sh
+./load_gg_profile.sh
+
+cd ${cur_dir}
+screen -S belt -h 200 -d -m python \
+-m master.ggd.belt belt_ggd \
+~/mini-fulfillment/groups/master/cfg.json \
+~/mini-fulfillment/groups/master/ggd/certs/root-ca.pem \
+~/mini-fulfillment/groups/master/ggd/certs/belt_ggd.pem \
+~/mini-fulfillment/groups/master/ggd/certs/belt_ggd.prv \
+~/mini-fulfillment/groups/master/ggd/certs
 screen -S bridge -h 200 -d -m python -m master.ggd.bridge master/cfg.tmp.json
-screen -S button -h 200 -d -m python -m master.ggd.button master/cfg.tmp.json box
-screen -S heartbeat -h 200 -d -m python -m master.ggd.heartbeat master/cfg.tmp.json
+screen -S button -h 200 -d -m python \
+-m master.ggd.button button_ggd \
+~/mini-fulfillment/groups/master/cfg.json \
+~/mini-fulfillment/groups/master/ggd/certs/root-ca.pem \
+~/mini-fulfillment/groups/master/ggd/certs/button_ggd.pem \
+~/mini-fulfillment/groups/master/ggd/certs/button_ggd.prv \
+~/mini-fulfillment/groups/master/ggd/certs
+screen -S heartbeat -h 200 -d -m python \
+-m master.ggd.heartbeat heartbeat_ggd \
+~/mini-fulfillment/groups/master/cfg.json \
+~/mini-fulfillment/groups/master/ggd/certs/root-ca.pem \
+~/mini-fulfillment/groups/master/ggd/certs/heartbeat_ggd.pem \
+~/mini-fulfillment/groups/master/ggd/certs/heartbeat_ggd.prv \
+~/mini-fulfillment/groups/master/ggd/certs --frequency 0.1
 screen -S web -h 200 -d -m python -m master.ggd.web master/cfg.tmp.json
 screen -ls
-cd master
