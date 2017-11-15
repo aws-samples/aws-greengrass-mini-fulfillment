de0379dd5b711add46946570382e62cf92818ad5
Brett Francis <brettf@amazon.com>
2017-11-14 16:21:40 -0800

    fixed cd error

--- groups/master/start_master.sh
+++ groups/master/start_master.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-cur_dir=$(pwd)
+master_dir=$(pwd)
 
 cd ggd
 chmod 755 load_gg_profile.sh
 ./load_gg_profile.sh
 
-cd ${cur_dir}
+cd ${master_dir}
+cd ..
 screen -S belt -h 200 -d -m python \
 -m master.ggd.belt belt_ggd \
 ~/mini-fulfillment/groups/master/cfg.json \
@@ -14,7 +15,12 @@ screen -S belt -h 200 -d -m python \
 ~/mini-fulfillment/groups/master/ggd/certs/belt_ggd.pem \
 ~/mini-fulfillment/groups/master/ggd/certs/belt_ggd.prv \
 ~/mini-fulfillment/groups/master/ggd/certs
-screen -S bridge -h 200 -d -m python -m master.ggd.bridge master/cfg.tmp.json
+screen -S bridge -h 200 -d -m python -m master.ggd.bridge bridge_ggd \
+~/mini-fulfillment/groups/master/cfg.json \
+~/mini-fulfillment/groups/master/ggd/certs/root-ca.pem \
+~/mini-fulfillment/groups/master/ggd/certs/bridge_ggd.pem \
+~/mini-fulfillment/groups/master/ggd/certs/bridge_ggd.prv \
+~/mini-fulfillment/groups/master/ggd/certs
 screen -S button -h 200 -d -m python \
 -m master.ggd.button button_ggd \
 ~/mini-fulfillment/groups/master/cfg.json \
@@ -37,3 +43,4 @@ screen -S web -h 200 -d -m python \
 ~/mini-fulfillment/groups/master/ggd/certs/web_ggd.prv \
 ~/mini-fulfillment/groups/master/ggd/certs
 screen -ls
+cd ${master_dir}
